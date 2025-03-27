167944144fa658dea095d77ad2e06c372c374836
laboratorio <laboratorio@UPOAULA10713>
2025-03-27 08:24:08 -0500

    Agregar archivos de proyecto.

---
+++ RodriguezJean_MiPrimerProyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RodriguezJean_MiPrimerProyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
